55dade71ba8897dd6462f5be72263e64fe8e7fca
PratikW31 <112165801+PratikW31@users.noreply.github.com>
2022-11-10 10:25:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,99 @@
 import streamlit as st
-import numpy as np
+
 import pandas as pd
-
-
-
-
-st.title("Pratik Wable")
-
-st.write("MscDSAI")
-
-st.header("Python")
-
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
-
-st.caption('This is a string that explains something above.')
-
-st.text('This is some text.')
-
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-
-st.code(code, language='python')
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
-
-st.table(df)
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.area_chart(chart_data)
-chart_data = pd.DataFrame(
-    np.random.randn(50, 3),
-    columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
-import matplotlib.pyplot as plt
 import numpy as np
+from collections import Counter
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import altair as alt
 
-st.pyplot(fig)
+
+
+
+
+def main():
+    st.title("Data Visualization")
+    df = pd.read_csv("data/StudentsPerformance.csv")
+    
+
+    st.dataframe(df.head())
+
+    #df['species'].value_counts().plot(kind="bar")
+    #st.pyplot()
+
+    #Bar chart
+    #using St.bar_chart
+
+    #st.bar_chart(df[['sepal_length','petal_length']])
+
+    #lang_list = df2.columns.tolist()
+
+    #lang_choices = st.multiselect("Choose Language", lang_list, default="Python")
+    #new_df = df2[lang_choices]
+    #st.line_chart(new_df)
+
+    #Area chart
+
+    # st.area_chart(new_df)
+
+
+    fig = px.pie(df,'gender', title="Pie chart of gender")
+    st.plotly_chart(fig)
+
+    count = Counter(df['race/ethnicity'])
+    data = pd.DataFrame({'Ethnicity group':count.keys(), 'Number of students':count.values()})
+    fig = px.bar(data, 'Ethnicity group', 'Number of students', title="Race/Ethnicity")
+    st.plotly_chart(fig)
+
+
+    fig = px.scatter(df, 'math score', 'reading score',color="gender", title="Math and Reading score")
+    st.plotly_chart(fig)
+
+
+
+    clist = ["reading score", "math score","writing score"]
+
+    parents = st.multiselect("Select education", clist, default="math score")
+    data = df[parents]
+    st.line_chart(data)
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+    
+
+    
+
+
+
+
+    #fig = plt.figure()
+    #df['species'].value_counts().plot(kind="bar")
+    #st.pyplot(fig)
+
+    #method 2
+    #fig,ax = plt.subplots()
+    #df['species'].value_counts().plot(kind="bar")
+    #st.pyplot(fig)
+    
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
